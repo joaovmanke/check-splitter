d5b613ee0a7ca07d8ccd925b2e4e1a50485e0389
João Victor Manke <joaovmanke@gmail.com>
2019-02-11 15:57:18 -0200

    Added a prompt to input name of newly created divider

--- src/components/DividerCreator.js
+++ src/components/DividerCreator.js
@@ -1,13 +1,28 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import Swal from "sweetalert2";
 
 import { newDivider } from "../actions";
 
 import "./css/DividerCreator.css";
 
 class DividerCreator extends Component {
-  promptDividerName() {
-    this.props.newDivider("John");
+  async promptDividerName() {
+    const { value: name } = await Swal.fire({
+      input: "text",
+
+      title: "<span style='color: white'>Enter name:</span>",
+      background: "var(--DarkGray)",
+
+      confirmButtonText: "Confirm",
+      confirmButtonColor: "var(--mainOrange)",
+
+      customClass: "divider-creator__prompt"
+    });
+
+    if (name) {
+      this.props.newDivider(name);
+    }
   }
   render() {
     return (
